bacaa9555e87927f44c240644ad48bc9f6a8e0e7
Alexande Juca <corextechnologies@gmail.com>
2020-05-17 20:31:08 +0100

    validate configuration

--- lib/okta_auth.ex
+++ lib/okta_auth.ex
@@ -2,18 +2,25 @@ defmodule OktaAuth do
   use ExOAuth2.Strategy
 
   def client do
+    config = :okta_auth
+             |> Application.fetch_env!(__MODULE__)
+             |> validate_config!(:client_id)
+             |> validate_config!(:client_secret)
+             |> validate_config!(:site)
+             |> validate_config!(:redirect_uri)
+      
     
-      site = System.get_env("OKTA_DOMAIN")
-      ExOAuth2.Client.new([
-          strategy: __MODULE__,
-          client_id: System.get_env("OKTA_CLIENT_ID"),
-          client_secret: System.get_env("OKTA_CLIENT_SECRET"),
-          site: site,
-          redirect_uri: System.get_env("OKTA_REDIRECT_URI"),
-          authorize_url: site <> "/v1/authorize",
-          token_url: site <> "/v1/token"
-      ])
-      |> ExOAuth2.Client.put_serializer("application/json", Jason)
+    site = Keyword.get(config, :site)
+    ExOAuth2.Client.new([
+      strategy: __MODULE__,
+      client_id: Keyword.get(config, :client_id),
+      client_secret: Keyword.get(config, :client_secret),
+      site: site,
+      redirect_uri: Keyword.get(config, :redirect_uri),
+      authorize_url: site <> "/v1/authorize",
+      token_url: site <> "/v1/token"
+    ])
+    |> ExOAuth2.Client.put_serializer("application/json", Jason)
   end
 
   def authorize_url! do
@@ -66,15 +73,18 @@ defmodule OktaAuth do
       |> IO.inspect
   end
 
-  defp validate_config!(_, _) do: raise "Config: okta_auth, OktaAuth, expects a keyword list" end
+  defp validate_config!(_, _) do
+     raise "config: okta_auth, OktaAuth, expects a keyword list"
+  end 
 
   defp validate_config!(config, key) when is_list(config) do
     with val when is_bitstring(val) <- Keyword.get(config, key),
-         {^key, true} <- if (key == :site, do: {key, String.starts_with?(val, "http")}, else: {key, val != "" })
+         {^key, true} <- if(key == :site, do: {key, String.starts_with?(val, "http")}, else: {key, val != ""})
     do
       config
     else
       false -> raise "#{inspect(key)} in okta_auth, OktaAuth, must be a bitstring"
+      {:redirect_uri, false} -> raise ":redirect_uri in config okta_auth, OktaAuth, is not a valid url"
       {:site, false} -> raise ":site in config okta_auth, OktaAuth, is not a valid url"
       {key, false} -> raise "#{inspect(key)} in okta_auth, OktaAuth, is an empty string"
     end
