18b446e6618323a5450cb2c31975104b9a4d98c5
Alexande Juca <corextechnologies@gmail.com>
2020-05-19 15:02:06 +0100

    Added application config for test suite

---
+++ test/config.ex
@@ -0,0 +1,7 @@
+import Config
+
+config :ex_okta_auth, ExOktaAuth.Okta,
+    client_id: "isoaspoaisa",
+    client_secret: "kajskaljs",
+    site: "http://127.0.0.1:4000/default",
+    redirect_uri: "https://your-apps-callback-uri"
